827c93e06c3c7dd91086ddc5ba83ddb758d1ff8e
fg51 <zknsftk1msrk@gmail.com>
2023-11-13 11:28:21 +0900

    fix

--- py-work/src/gausian_filter.py
+++ py-work/src/gausian_filter.py
@@ -14,15 +14,16 @@ def generate_gaussian_kernel(
     sigma: std dev of gauss for kernel
     """
 
-    assert (kernel_width % 2 == 1) and (kernel_height % 2 == 1)
+    width_half, height_half = kernel_width // 2, kernel_height // 2
+    assert (width_half == 1) and (height_half == 1)
 
     kernel = np.empty((kernel_height, kernel_width))
 
-    for y in range(-(kernel_height // 2), kernel_height // 2 + 1):
-        for x in range(-(kernel_width // 2), kernel_width // 2 + 1):
+    for y in range(-height_half, height_half + 1):
+        for x in range(-width_half, width_half + 1):
             h = -(x * x + y * y) / (2.0 * sigma * sigma)
             h = np.exp(h) / (2.0 * np.pi * sigma * sigma)
-            kernel[y + kernel_height // 2, x + kernel_width // 2] = h
+            kernel[y + height_half, x + width_half] = h
     kernel /= np.sum(kernel)  # normalize : sum is 1
     return kernel
 
@@ -31,12 +32,13 @@ def convolution(img: Image.Image, kernel: np.ndarray, x: int, y: int):
     width, height = img.size
     kernel_height, kernel_width = kernel.shape[:2]
 
+    width_half, height_half = kernel_width // 2, kernel_height // 2
     value = 0
-    for y_kernel in range(-(kernel_height // 2), kernel_height // 2 + 1):
-        for x_kernel in range(-(kernel_width // 2), kernel_width // 2 + 1):
+    for y_kernel in range(-height_half, height_half + 1):
+        for x_kernel in range(-width_half, width_half + 1):
             x_img = max(min(x + x_kernel, width - 1), 0)
             y_img = max(min(y + y_kernel, height - 1), 0)
-            h = kernel[y_kernel + kernel_height // 2, x_kernel + kernel_width // 2]
+            h = kernel[y_kernel + height_half, x_kernel + width_half]
             value += h * img.getpixel((x_img, y_img))
     return value
 
